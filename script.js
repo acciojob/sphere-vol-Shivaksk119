b77782660fe9e59059436a0765662cc8c9ac0e18
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-25 22:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function volume_sphere() {
+
+
+function volume_sphere(event) {
     //Write your code here
+	event.preventDefault();
 	let rad = document.getElementById('radius').value;
+
+	if(isNaN(rad) || rad<0){
+		document.getElementById('volume').value = 'NaN';
+		return;
+	}
 	let vol = (4/3) * (Math.PI) * (Math.pow(rad,3));
+	vol = vol.toFixed(4);
 	document.getElementById('volume').value = vol;
   
 } 
